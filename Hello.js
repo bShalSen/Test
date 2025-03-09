995e1314a852f434e87381941f9e4c5964da67dd
Bishal <bishalsenwork@gmail.com>
2025-03-09 12:16:49 +0530

    The First proper push

---
+++ Hello.js
@@ -0,0 +1,18 @@
+// A simple JavaScript program to say hello to Git
+
+// Function to create a greeting message
+function greetGit() {
+    const message = "Hello, Git!";
+    console.log(message);
+    
+    // Display more information
+    console.log("This is my first Git repository.");
+    console.log("I'm learning to use Git for version control.");
+    
+    return message;
+  }
+  
+  // Call the function
+  greetGit();
+  
+  // If you're using this in a browser, you can also display the message in the DO
